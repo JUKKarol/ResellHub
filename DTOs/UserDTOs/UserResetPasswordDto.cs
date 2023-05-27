68fe166018b90e579c19adf4076bca3d0b2a15a4
Karol Przybylak <karolprzybylak2@gmail.com>
2023-05-27 22:34:09 +0200

    Upgrade dto validation

--- DTOs/UserDTOs/UserResetPasswordDto.cs
+++ DTOs/UserDTOs/UserResetPasswordDto.cs
@@ -6,7 +6,9 @@ namespace ResellHub.DTOs.UserDTOs
     {
         [Required]
         public string Token { get; set; }
-        [Required, MinLength(6)]
+        [Required]
+        [StringLength(30, ErrorMessage = "Password can be up to 30 characters long")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
         public string Password { get; set; }
         [Required, Compare("Password")]
         public string ConfirmPassword { get; set; }
