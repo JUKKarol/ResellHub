d3e35a624cc2576afb9773cc7d4c423f1a9c6c92
JUKKarol <karolprzybylak2@gmail.com>
2023-08-21 21:48:53 +0200

    Add Sieve to project

---
+++ api/Utilities/Sieve/ApplicationSieveProcessor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Options;
+using Sieve.Models;
+using Sieve.Services;
+
+namespace ResellHub.Utilities.Sieve
+{
+    public class ApplicationSieveProcessor : SieveProcessor
+    {
+        public ApplicationSieveProcessor(IOptions<SieveOptions> options) : base(options)
+        {
+        }
+    }
+}
